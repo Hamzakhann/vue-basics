b4449fd72ebf0557ff656f70bbca39f5213fa187
Hamzakhann <hamzakhann66@gmail.com>
2022-07-31 22:18:32 +0500

    learnt a lot

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :heading="heading" :text="text" theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <p>This is Slot</p>
+    </Modal>
+
+  </div>
+  <button @click.alt="toggleModal">open modal (alt click)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App!',
+      heading: 'Sign up for the Givaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
@@ -22,5 +43,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
